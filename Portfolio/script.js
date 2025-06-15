94b6b83fde479e0fbf8c941427f70de08e2ee8ec
PranaysinghShengar <145133116+PranaysinghShengar@users.noreply.github.com>
2025-06-15 17:26:27 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,53 @@
+// Resume Button
+
+document.getElementById("Resumebtn").onclick = function () {
+    window.location.href = "Resume.html";
+};
+
+// About Button
+
+document.getElementById("aboutbtn").onclick = function() {
+    window.location.href = "About.html";
+};
+
+// Education Buttons
+
+document.getElementById("Degree").onclick = function() {
+    window.location.href = "degree.html";
+};
+
+document.getElementById("diploma1").onclick = function() {
+    window.location.href = "diploma1.html";
+};
+
+document.getElementById("diploma2").onclick = function() {
+    window.location.href = "diploma2.html";
+};
+
+// Project Buttons
+
+document.getElementById("megaproject").onclick = function() {
+    window.location.href = "megaproject.html";
+};
+
+document.getElementById("landing").onclick = function() {
+    window.location.href = "landing.html";
+};
+
+document.getElementById("portfolio").onclick = function() {
+    window.location.href = "portfolio.html";
+};
+
+document.getElementById("carvilla").onclick = function() {
+    window.location.href = "carvilla.html";
+};
+
+// Certificates Buttons
+
+document.getElementById("touch").onclick = function() {
+    window.location.href = "touch.html";
+};
+
+document.getElementById("arcane").onclick = function() {
+    window.location.href = "arcane.html";
+};
